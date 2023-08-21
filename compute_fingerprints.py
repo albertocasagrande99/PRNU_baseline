3fae45d2e56c5b703989fa2475d03094fa078830
albertocasagrande99 <alberto.casagrande@studenti.unitn.it>
2023-08-21 12:36:00 +0200

    Update compute_fingerprints.py

--- compute_fingerprints.py
+++ compute_fingerprints.py
@@ -27,6 +27,7 @@ import sys
 import matplotlib.pyplot as plt
 from sklearn import metrics
 import tqdm
+import random
 
 
 def main():
@@ -36,22 +37,19 @@ def main():
     Check the detection performance obtained with cross-correlation and PCE
     :return:
     """
-
-    '''fingerprint_devices = os.listdir("test/Dataset/")
+    fingerprint_devices = os.listdir("test/Videos/")
     fingerprint_devices = sorted(np.unique(fingerprint_devices))
-    
-    fingerprint_devices.remove('.DS_Store')'''
-
-    fingerprint_devices = os.listdir("video_fingerprints/")
-    fingerprint_devices = sorted(np.unique(fingerprint_devices))
-    
     fingerprint_devices.remove('.DS_Store')
+    
     k = []   #fingerprints of the cameras
     for device in fingerprint_devices:
-        print(device)
+        print("Computing fingerprint of " + device)
         if (device != ".DS_Store"):
             imgs = []
-            ff_dirlist = np.array(sorted(glob('video_fingerprints/' + device + '/Flat/*.jpg')))
+            #ff_dirlist = np.array(sorted(glob('test/Dataset/' + device + '/Images/Flat/JPG/*.jpg')))     #for images
+            ff_dirlist = np.array(sorted(glob('test/Videos/' + device + '/Videos/VideoLevel+/Train/*.jpg')))      #for videos
+            #random.shuffle(ff_dirlist)
+            #ff_dirlist = ff_dirlist[:50]
             for img_path in tqdm.tqdm(ff_dirlist):
                 im = Image.open(img_path)
                 im_arr = np.asarray(im)
@@ -64,7 +62,7 @@ def main():
                 im_cut = prnu.cut_ctr(im_arr, (512, 512, 3))
                 imgs += [im_cut]
             k += [prnu.extract_multiple_aligned(imgs, processes=cpu_count())]
-    np.save("video_fingerprint512x512.npy", k)
+    np.save("512x512_VideoLevel+.npy", k)
 
 if __name__ == '__main__':
     main()
